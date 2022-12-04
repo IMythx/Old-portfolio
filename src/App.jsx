44b4722824fde39b6fbd17e7ce76161c316996e1
IMythx <mahmoudhelal9888@gmail.com>
2022-12-05 00:04:27 +0200

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from "./pages/About/About";
 import Portfolio from "./pages/portfolio/Portfolio";
 import Contact from "./pages/contact/Contact";
 const App = () => {
-  const { isTablet, isMobile } = useQuery();
+  const { isTablet } = useQuery();
   return (
     <div className="App">
       {!isTablet && <Cursor />}
